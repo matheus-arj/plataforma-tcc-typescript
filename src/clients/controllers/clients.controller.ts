79ab5d68d80589d07f1cf17f5acda40f006a3f19
matheus-arj <matheus.arj@hotmail.com>
2025-04-08 12:07:13 -0300

    feat: added get by id method on controller

--- src/clients/controllers/clients.controller.ts
+++ src/clients/controllers/clients.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { FindClientsUseCase } from '../use-cases/find-clients.use-case';
+import { FindClientUseCase } from '../use-cases/find-client.use-case';
 
 @Controller('clients')
 export class ClientsController {
   @Inject(FindClientsUseCase)
   private readonly findClientsUseCase: FindClientsUseCase;
+  @Inject(FindClientUseCase)
+  private readonly findClientUseCase: FindClientUseCase;
 
   @Get()
   public async findAll() {
     return this.findClientsUseCase.execute();
+  }
+
+  @Get(':id')
+  public async findById(@Param('id') id: string) {
+    return this.findClientUseCase.execute(id);
   }
 }
