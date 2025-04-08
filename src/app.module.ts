22c6411106e9f1454029c7e3b7e591f48a67fbb1
matheus-arj <matheus.arj@hotmail.com>
2025-04-08 11:49:47 -0300

    feat: added prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
 import { ClientsController } from './clients/controllers/clients.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ClientsModule],
   controllers: [AppController, ClientsController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
+  exports: [AppService, PrismaService],
 })
 export class AppModule {}
