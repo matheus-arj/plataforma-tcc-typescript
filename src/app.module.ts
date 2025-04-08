567eb1ad8a7f927475277f84d01518fa4de83a9a
matheus-arj <matheus.arj@hotmail.com>
2025-04-08 10:59:40 -0300

    feat: added clients module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { ClientsController } from './clients/controllers/clients.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ClientsModule],
+  controllers: [AppController, ClientsController],
   providers: [AppService],
 })
 export class AppModule {}
