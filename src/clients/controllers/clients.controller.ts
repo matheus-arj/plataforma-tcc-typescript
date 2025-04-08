6f77304d38af7ac515e23315d81d997a351c0583
matheus-arj <matheus.arj@hotmail.com>
2025-04-08 13:43:24 -0300

    feat: created post api to create new client

--- src/clients/controllers/clients.controller.ts
+++ src/clients/controllers/clients.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
-import { FindClientsUseCase } from '../use-cases/find-clients.use-case';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { CreateClientDto } from '../dtos/create-client.dto';
+import { CreateClientUseCase } from '../use-cases/create-client.use-case';
 import { FindClientUseCase } from '../use-cases/find-client.use-case';
+import { FindClientsUseCase } from '../use-cases/find-clients.use-case';
 
 @Controller('clients')
 export class ClientsController {
@@ -8,6 +10,8 @@ export class ClientsController {
   private readonly findClientsUseCase: FindClientsUseCase;
   @Inject(FindClientUseCase)
   private readonly findClientUseCase: FindClientUseCase;
+  @Inject(CreateClientUseCase)
+  private readonly createClientUseCase: CreateClientUseCase;
 
   @Get()
   public async findAll() {
@@ -17,5 +21,10 @@ export class ClientsController {
   @Get(':id')
   public async findById(@Param('id') id: string) {
     return this.findClientUseCase.execute(id);
+  }
+
+  @Post()
+  public async create(@Body() data: CreateClientDto) {
+    return await this.createClientUseCase.execute(data);
   }
 }
