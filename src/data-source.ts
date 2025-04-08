4837b85221f2ce633e6c2809b5468d3a42b5b407
matheus-arj <matheus.arj@hotmail.com>
2025-04-08 11:03:32 -0300

    feat: created data source to create connection

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+const connectionOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: true,
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migrations/*.ts'],
+};
+
+export default connectionOptions;
