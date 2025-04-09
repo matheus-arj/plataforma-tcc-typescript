d53acfbb6372e6ba0aad6866b1c268992f10743c
matheus-arj <matheus.arj@hotmail.com>
2025-04-09 17:46:52 -0300

    feat: added logs to all use cases

--- src/clients/use-cases/delete-client.use-case.ts
+++ src/clients/use-cases/delete-client.use-case.ts
@@ -1,13 +1,32 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Clients } from '@prisma/client';
 import { ClientsRepository } from '../clients.repository';
 
 @Injectable()
 export class DeleteClientUseCase {
+  private readonly logger = new Logger(DeleteClientUseCase.name);
+
   @Inject(ClientsRepository)
   private readonly clientsRepository: ClientsRepository;
 
   public async execute(id: string): Promise<Clients> {
-    return await this.clientsRepository.delete(id);
+    await this.verifyClientIfExists(id);
+
+    this.logger.log(`Deleting client with id: ${id}`);
+    const deletedClient = await this.clientsRepository.delete(id);
+    this.logger.log(`Client deleted: ${JSON.stringify(deletedClient)}`);
+
+    return deletedClient;
+  }
+
+  private async verifyClientIfExists(id: string): Promise<void> {
+    this.logger.log(`Verifying if client with id: ${id} exists`);
+    const client = await this.clientsRepository.findById(id);
+    this.logger.log(`Client found: ${JSON.stringify(client)}`);
+
+    if (!client) {
+      this.logger.error(`Client with id ${id} not found`);
+      throw new NotFoundException(`Client with id ${id} not found`);
+    }
   }
 }
--- src/clients/use-cases/find-clients.use-case.ts
+++ src/clients/use-cases/find-clients.use-case.ts
@@ -1,12 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientsRepository } from '../clients.repository';
 
 @Injectable()
 export class FindClientsUseCase {
+  private readonly logger = new Logger(FindClientsUseCase.name);
+
   @Inject(ClientsRepository)
   private readonly clientsRepository: ClientsRepository;
 
   public async execute() {
+    this.logger.log('Finding all clients');
     return this.clientsRepository.findAll();
   }
 }
