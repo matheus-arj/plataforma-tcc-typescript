86df649b0c335953c6fd215ec335a039a0668487
matheus-arj <matheus.arj@hotmail.com>
2025-04-12 13:02:17 -0300

    feat: created method to create new product

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Products } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
-
+import { CreateProductDto } from './dto/create-product.dto';
 @Injectable()
 export class ProductsRepository {
   @Inject(PrismaService)
@@ -10,9 +10,16 @@ export class ProductsRepository {
   public async findAll(): Promise<Products[]> {
     return await this.$db.products.findMany();
   }
+
   public async findById(id: string) {
     return await this.$db.products.findUnique({
       where: { id },
+    });
+  }
+
+  public async create(data: CreateProductDto): Promise<Products> {
+    return await this.$db.products.create({
+      data,
     });
   }
 }
