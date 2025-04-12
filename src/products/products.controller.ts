fffc6eb19644aa61f32066fb4b16c52b374bb43a
matheus-arj <matheus.arj@hotmail.com>
2025-04-12 12:58:38 -0300

    feat: created post api to create new product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { CreateProductUseCase } from './use-cases/create-product.use-case';
 import { FindProductUseCase } from './use-cases/find-product.use-case';
 import { FindProductsUseCase } from './use-cases/find-products.use-case';
 
@@ -8,6 +10,8 @@ export class ProductsController {
   private readonly $findProductsUseCase: FindProductsUseCase;
   @Inject(FindProductUseCase)
   private readonly $findProductUseCase: FindProductUseCase;
+  @Inject(CreateProductUseCase)
+  private readonly $createProductUseCase: CreateProductUseCase;
 
   @Get()
   findAll() {
@@ -17,5 +21,10 @@ export class ProductsController {
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.$findProductUseCase.execute(id);
+  }
+
+  @Post()
+  create(@Body() data: CreateProductDto) {
+    return this.$createProductUseCase.execute(data);
   }
 }
