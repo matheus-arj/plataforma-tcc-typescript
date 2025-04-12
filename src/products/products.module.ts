56a3385a7c52d8e142f88a79a272e7da887c6863
matheus-arj <matheus.arj@hotmail.com>
2025-04-12 12:58:54 -0300

    feat: added create product use case on module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,14 +2,23 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AppModule } from 'src/app.module';
 import { ProductsController } from './products.controller';
 import { ProductsRepository } from './products.repository';
-
+import { CreateProductUseCase } from './use-cases/create-product.use-case';
 import { FindProductUseCase } from './use-cases/find-product.use-case';
 import { FindProductsUseCase } from './use-cases/find-products.use-case';
-
 @Module({
   imports: [forwardRef(() => AppModule)],
   controllers: [ProductsController],
-  providers: [ProductsRepository, FindProductsUseCase, FindProductUseCase],
-  exports: [ProductsRepository, FindProductsUseCase, FindProductUseCase],
+  providers: [
+    ProductsRepository,
+    FindProductsUseCase,
+    FindProductUseCase,
+    CreateProductUseCase,
+  ],
+  exports: [
+    ProductsRepository,
+    FindProductsUseCase,
+    FindProductUseCase,
+    CreateProductUseCase,
+  ],
 })
 export class ProductsModule {}
