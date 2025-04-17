6c41eb095769a494644aa9f5bb148e7db546927c
matheus-arj <matheus.arj@hotmail.com>
2025-04-16 21:45:32 -0300

    feat: added promise and condition if not found a product

--- src/products/use-cases/find-product.use-case.ts
+++ src/products/use-cases/find-product.use-case.ts
@@ -1,4 +1,5 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Products } from '@prisma/client';
 import { ProductsRepository } from '../products.repository';
 
 @Injectable()
@@ -8,8 +9,15 @@ export class FindProductUseCase {
   @Inject(ProductsRepository)
   private readonly $productsRepository: ProductsRepository;
 
-  public async execute(id: string) {
+  public async execute(id: string): Promise<Products> {
     this.$logger.log(`Finding product with id: ${id}`);
-    return this.$productsRepository.findById(id);
+    const product = await this.$productsRepository.findById(id);
+
+    if (!product) {
+      this.$logger.error(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
   }
 }
